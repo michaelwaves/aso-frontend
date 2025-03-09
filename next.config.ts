c5593fc7e95ed0e29b1abe9fcecc14fd64cce155
michaelwaves <michaelyu713705@gmail.com>
2025-03-09 19:49:56 -0400

    eslint fixes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  "rules": {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "prefer-const": "warn"
+  }
 };
 
 export default nextConfig;
